351c1aa52cff9fad2d009deed6797cd64869375d
Ana-Maria <anamariaolujic2208@gmail.com>
2024-01-25 18:41:03 +0100

    second commit

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,14 +1,26 @@
-import { useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getRecipeData } from "../../services/api";
 
 export default function Details() {
+  const [recipe, setRecipe] = useState({});
+  useEffect(() => {
+    debugger;
+    const fetchRecipe = async () => {
+      const data = await getRecipeData(id);
+      console.log(data);
+      setRecipe(data);
+    };
+    fetchRecipe();
+  }, []);
 
   let { id } = useParams();
-
 
   return (
     <>
       <div>Details</div>
       <p>Recipe id : {id}</p>
+      <p>{recipe && recipe.method}</p>
     </>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,22 @@
+import axios from "axios";
 
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = "http://localhost:3000";
 
 export const getAllRecipes = async () => {
-    try {
-        const response = await axios.get(BASE_URL + '/recipes');
-        return response
-      } catch (error) {
-        console.error(error);
-      }
-}
+  debugger;
+  try {
+    const response = await axios.get(BASE_URL + "/recipes");
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
+export const getRecipeData = async (id) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/recipes/${id}`);
+    return response.data;
+  } catch (error) {
+    confirm.error(error);
+  }
+};
